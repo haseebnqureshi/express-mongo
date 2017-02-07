93646b109aca848d0071766f0e62a651cc97ce7a
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-02-07 11:14:17 -0500

    enabling cors fully for every request

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ var bodyParser = require('body-parser');
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+
+	// allow cors
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
 
 // routes
 app = require('./mongo-crud.js')('items', app, express);
